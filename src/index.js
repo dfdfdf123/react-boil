1f38b5413967c28fb42275cee89d4ce4497180f6
johnromanoff <johnromanoff@yandex.ru>
2019-02-25 03:42:56 +0300

    update boil

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -7,10 +7,12 @@ import store from './store';
 import App from './components/App';
 
 ReactDOM.render(
-    <BrowserRouter>       
-        <Provider store={store}>
-            <App />
-        </Provider>
+    <BrowserRouter>
+        <Fragment>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Fragment>
     </BrowserRouter>,
     document.getElementById('root')
 );
